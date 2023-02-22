292a20224be019c11cea80188cd2a000626880bc
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-02-22 09:52:37 -0800

    Add refactor edits to button for conditionals

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,18 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass = "button";
+   let buttonClass = classNames("button", {
+      "button--confirm": props.confirm, 
+      "button--danger": props.danger
+   });
 
-   if (props.confirm) {
-      buttonClass += " button--confirm";
-   }
-
-   if (props.danger) {
-      buttonClass += " button--danger";
-   }
-
-   return <button className={buttonClass} disabled={props.disabled} onClick={props.onClick}>{props.children}</button>;
+   return (<button 
+      className={buttonClass} 
+      disabled={props.disabled} 
+      onClick={props.onClick}>
+         {props.children}
+      </button>);
 }
