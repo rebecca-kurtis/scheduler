19ee3de3a1e5bc4f2d3f6fcb14e6a0fc1d68ad04
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-02-26 19:41:31 -0800

    Add Error and Status components

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,8 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 
 storiesOf("Button", module)
@@ -170,4 +172,10 @@ storiesOf("InterviewerList", module)
       onConfirm={action("onConfirm")}
       onCancel={action("onCancel")}
       />
+    ))
+    .add("Status", () => <Status message="Deleting" />)
+    .add("Error", () => (
+      <Error 
+      message="Could not delete appointment."
+      onClose={action("onClose")}/>
     ))
