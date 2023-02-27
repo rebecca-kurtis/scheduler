847386970654dda4f3f6d83e346fb53e856b480a
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-02-26 19:33:49 -0800

    Add Show and Confirm components

--- stories/index.js
+++ stories/index.js
@@ -5,6 +5,7 @@ import { action } from "@storybook/addon-actions";
 
 import "index.scss";
 
+//Components imported
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
@@ -13,6 +14,9 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -151,4 +155,19 @@ storiesOf("InterviewerList", module)
     ))
     .add("Empty", () => (
       <Empty onAdd={action("onAdd")}/>
+    ))
+    .add("Show", () => (
+      <Show 
+      student="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+      />
+    ))
+    .add("Confirm", () => (
+      <Confirm 
+      message="Delete the appointment?"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
+      />
     ))
